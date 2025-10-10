4b3c5907ff47f4a3eb4b0ee151e8213b9b482c51
Kevin Mendoza <kevin.devcode@gmail.com>
2025-10-09 21:01:31 -0400

    ajuste app routes

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -5,6 +5,7 @@ import { RaffleComponent } from './components/raffle/raffle.component';
 import { SuccessComponent } from './components/success/success.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
+import { registrationGuard } from './guards/registration.guard';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { CameraScanDocumentComponent } from './components/camera-scan-document/camera-scan-document.component';
 
@@ -12,8 +13,8 @@ export const APP_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'camera-scan', component: CameraScanDocumentComponent, canActivate: [authGuard] },
-  { path: 'raffle', component: RaffleComponent, canActivate: [authGuard] },
+  { path: 'camera-scan', component: CameraScanDocumentComponent, canActivate: [registrationGuard] },
+  { path: 'raffle', component: RaffleComponent, canActivate: [registrationGuard] },
   { path: 'success', component: SuccessComponent, canActivate: [authGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   
